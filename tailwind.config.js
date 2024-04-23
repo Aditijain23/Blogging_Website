48a2a3cf2de17fbc7dba60ab4643802b7c19f536
Aditijain23 <103481389+Aditijain23@users.noreply.github.com>
2024-04-23 09:14:43 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1000px'},
+      // => @media (max-width: 1000px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    },
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
